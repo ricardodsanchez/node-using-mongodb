b8bb29a9360a5848d81fa8ae871ae81a8708217d
Ricardo Sánchez <ricardodsanchez@users.noreply.github.com>
2022-01-12 12:06:08 -0800

    Added mongoose connection

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const PORT = 4000;
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
 );
 
-app.listen(PORT, () => 
+app.listen(PORT, () =>
     console.log(`Your server is running on port ${PORT}`)
 );
+
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/productsdb", { useNewUrlParser: true, useUnifiedTopology: true });
